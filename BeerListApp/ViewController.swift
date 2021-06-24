db5480334cfd3008f69a2472352def54d03fa39f
dantmac <dantmac@mail.ru>
2021-06-24 14:18:33 +0300

    Initial Commit

---
+++ BeerListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeerListApp
+//
+//  Created by admin on 24.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
